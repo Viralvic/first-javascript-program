08b2f13199a4c3ed38f28db8aae36cb45047c222
viralvic <olukuv@gmail.com>
2022-06-20 20:19:20 +0100

    First commit

---
+++ intro.js
@@ -0,0 +1,12 @@
+//Variable declaration//
+let first_name = "Victory"
+let last_name = "Oluku"
+let height = 5.7 + "ft"
+let country = "Nigeria"
+
+//Printing with console.log//
+
+console.log("My name is "+"" + first_name);
+console.log("My name is "+"" + last_name);
+console.log("My name is "+"" + height);
+console.log("My name is "+"" + country);
